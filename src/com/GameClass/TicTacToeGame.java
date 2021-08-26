6aab982c8521757b2a875617f245e1ca339c00fc
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-26 16:09:51 +0530

    Added features that will allow user to make move and updated usecase2 playerLetter method

--- src/com/GameClass/TicTacToeGame.java
+++ src/com/GameClass/TicTacToeGame.java
@@ -18,9 +18,10 @@ public class TicTacToeGame {
 
 		TicTacToeGame tictactoe = new TicTacToeGame();
 		tictactoe.initBoard();
-		//tictactoe.playerLetter();
-		//tictactoe.showBoard();
+		tictactoe.playerLetter();
+		tictactoe.showBoard();
 		tictactoe.userMove();
+		tictactoe.showBoard();
 
 	}
 
@@ -41,17 +42,24 @@ public class TicTacToeGame {
 	 */
 
 	public void playerLetter() {
+		int flag=1;
+		do {
 		System.out.println("Choose your letter.I.e X or O");
 		Scanner r = new Scanner(System.in);
 		player1 = r.nextLine();
 		if (player1.equals("X")) {
 			player2 = "O";
-		} else if (player1.equals("O")) {
+			flag=1;
+		} 
+		else if (player1.equals("O")) {
 			player2 = "X";
-		} else {
+			flag=1;
+		} 
+		else {
 			System.out.println("Invalid input");
-
+			flag=0;
 		}
+		}while(flag==0);
 		System.out.println("Player 1 choose " + player1);
 		System.out.println("Player 2's Letter is " + player2);
 	}
@@ -92,25 +100,33 @@ public class TicTacToeGame {
 		int flag=1;
 		do
 		{
+			
 			System.out.println("Enter the index between 1 to 9 to which you wanna move");
 			index=r.nextInt();
 			if(index>9 || index <0) {
 				System.out.println("Invalid index.Choose between 1 to 9");
 				flag=0;
 			}
+			else
+				flag=1;
+			if(flag !=0) {
 			
 			if(board[index]==' ') {
+				board[index]=player1.toCharArray()[0];
 				System.out.println("Index is free and you can make move");
-				
+				flag=1;
 			}
 			else {
 				System.out.println("Index is full and you cannot make move to that index");
-				
+				flag=0;
+			}
 			}
 		}while(flag==0);
 		
 		
 	}
+	
+	
 
 	}
 
